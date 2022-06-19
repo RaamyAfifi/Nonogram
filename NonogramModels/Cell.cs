3e8aba0888963b9c3b46aef445d15e80895948b9
RaamyAfifi <56026512+RaamyAfifi@users.noreply.github.com>
2022-06-20 01:30:50 +0200

    added fields

--- NonogramModels/Cell.cs
+++ NonogramModels/Cell.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI;
 
 namespace NonogramModels
 {
     public class Cell
     {
+        private bool isCrossed;
+        private bool isColored;
+        private Color rgba = Colors.White;
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
--- NonogramModels/Puzzle.cs
+++ NonogramModels/Puzzle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,8 @@ namespace NonogramModels
 {
     public class Puzzle
     {
+        public ObservableCollection<ObservableCollection<Cell>> PuzzleSolution { get; set; } = new ObservableCollection<ObservableCollection<Cell>>();
+        public int XAxis { get; set; }
+        public int YAxis { get; set; }
     }
 }
