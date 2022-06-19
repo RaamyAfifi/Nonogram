2550273295eb89f84103ce7dd2f5dcb62cea84bb
RaamyAfifi <56026512+RaamyAfifi@users.noreply.github.com>
2022-06-20 01:46:36 +0200

    Update Cell and Puzzle

--- NonogramModels/Puzzle.cs
+++ NonogramModels/Puzzle.cs
@@ -12,5 +12,40 @@ namespace NonogramModels
         public ObservableCollection<ObservableCollection<Cell>> PuzzleSolution { get; set; } = new ObservableCollection<ObservableCollection<Cell>>();
         public int XAxis { get; set; }
         public int YAxis { get; set; }
+
+        public Puzzle(int xAxis, int yAxis)
+        {
+            XAxis = xAxis;
+            YAxis = yAxis;
+
+            InitPuzzle();
+        }
+
+        private void InitPuzzle()
+        {
+            for (int i = 0; i < YAxis; i++)
+            {
+                PuzzleSolution.Insert(i, new ObservableCollection<Cell>());
+                for (int j = 0; j < XAxis; j++)
+                {
+                    PuzzleSolution[i].Insert(j, new Cell());
+                }
+            }
+        }
+
+        public List<bool> GetBlackAndWhiteSolution()
+        {
+            var solution = new List<bool>();
+
+            for (int i = 0; i < YAxis; i++)
+            {
+                for (int j = 0; j < XAxis; j++)
+                {
+                    solution.Add(PuzzleSolution[i][j].IsColored);
+                }
+            }
+
+            return solution;
+        }
     }
 }
