417ba7354c6484a9881ad3b229bcae453af79dc3
maartjeA2001 <55484829+maartjeA2001@users.noreply.github.com>
2022-06-20 22:07:21 +0200

    cleanup unnecessary lines

--- Nonogram/ViewModels/DesignPageViewModel.cs
+++ Nonogram/ViewModels/DesignPageViewModel.cs
@@ -1,12 +1,7 @@
 ﻿using NonogramModels;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 using Windows.UI;
 using Microsoft.Toolkit.Mvvm.Input;
