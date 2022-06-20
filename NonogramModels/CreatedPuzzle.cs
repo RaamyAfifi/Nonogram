0f71098d9f501b7381ea943e6c313587df945ba6
maartjeA2001 <55484829+maartjeA2001@users.noreply.github.com>
2022-06-20 02:12:08 +0200

    add methods createdPuzzle + SolvingPuzzle

--- NonogramModels/CreatedPuzzle.cs
+++ NonogramModels/CreatedPuzzle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,5 +12,106 @@ namespace NonogramModels
         public Puzzle Puzzle { get; set; }
         public string Creator { get; set; }
         public string Description { get; set; }
+
+        public CreatedPuzzle(string creator, string description)
+        {
+            Creator = creator;
+            Description = description;
+        }
+
+        /// <summary>
+        /// Get hints of all columns
+        /// </summary>
+        /// <returns>List containing lists of hints of all columns</returns>
+        public List<List<int>> GetAllColumnHints()
+        {
+            var list = new List<List<int>>();
+            for (int i = 0; i < Puzzle.XAxis; i++)
+            {
+                list.Add(GetColumnHints(i));
+            }
+
+            return list;
+        }
+
+        /// <summary>
+        /// Get hints of all rows
+        /// </summary>
+        /// <returns>List containing lists of hints of all rows</returns>
+        public List<List<int>> GetAllRowHints()
+        {
+            var list = new List<List<int>>();
+            for (int i = 0; i < Puzzle.YAxis; i++)
+            {
+                list.Add(GetRowHints(i));
+            }
+
+            return list;
+        }
+
+        /// <summary>
+        /// Get hints of row
+        /// </summary>
+        /// <param name="row">index of the row</param>
+        /// <returns>List containing hint(s)</returns>
+        public List<int> GetRowHints(int row)
+        {
+            var cellRow = Puzzle.PuzzleSolution[row];
+
+            return GetHints(cellRow);
+        }
+
+        /// <summary>
+        /// Get hints of column
+        /// </summary>
+        /// <param name="column">index of the column</param>
+        /// <returns>List containing hint(s)</returns>
+        public List<int> GetColumnHints(int column)
+        {
+            var cellColumn = new ObservableCollection<Cell>();
+
+            foreach (var row in Puzzle.PuzzleSolution)
+            {
+                cellColumn.Add(row[column]);
+            }
+
+            return GetHints(cellColumn);
+        }
+
+        /// <summary>
+        /// Get hints of ObservableCollection
+        /// A hint is the amount of colored cells next to each other
+        /// A new hint is added to the list if colored cells are separated by an uncolored cell
+        /// returns list with 0 if no colored cells are in the collection
+        /// </summary>
+        /// <param name="collection">ObservableCollection of cells</param>
+        /// <returns>List containing the hint(s)</returns>
+        private static List<int> GetHints(ObservableCollection<Cell> collection)
+        {
+            var hints = new List<int>();
+            var hint = 0;
+            foreach (var cell in collection)
+            {
+                if (cell.IsColored)
+                {
+                    hint++;
+                }
+                else
+                {
+                    if (hint > 0)
+                    {
+                        hints.Add(hint);
+                        hint = 0;
+                    }
+                }
+            }
+
+            if (hint > 0 || hints.Count == 0)
+            {
+                hints.Add(hint);
+            }
+
+            return hints;
+        }
     }
 }
--- NonogramModels/SolvingPuzzle.cs
+++ NonogramModels/SolvingPuzzle.cs
@@ -10,5 +10,37 @@ namespace NonogramModels
     {
         public CreatedPuzzle SolvedPuzzle { get; set; }
         public Puzzle Puzzle { get; set; }
+
+        public SolvingPuzzle(CreatedPuzzle solvedPuzzle)
+        {
+            SolvedPuzzle = solvedPuzzle;
+            Puzzle = new Puzzle(SolvedPuzzle.Puzzle.XAxis, SolvedPuzzle.Puzzle.YAxis);
+        }
+
+        /// <summary>
+        /// Check if black and white solutions of puzzle en created puzzle are the same
+        /// </summary>
+        /// <returns>True if solution is right</returns>
+        public bool ValidateSolution()
+        {
+            bool result = SolvedPuzzle.Puzzle.GetBlackAndWhiteSolution()
+                .SequenceEqual(Puzzle.GetBlackAndWhiteSolution());
+            return result;
+        }
+
+        /// <summary>
+        /// Color the puzzle with the created puzzle's solution
+        /// </summary>
+        public void ColorSolution()
+        {
+            foreach (var row in Puzzle.PuzzleSolution.Select((value, x) => (value, x)))
+            {
+                foreach (var cell in row.value.Select((value, y) => (value, y)))
+                {
+                    cell.value.RGBA = SolvedPuzzle.Puzzle.PuzzleSolution[row.x][cell.y].RGBA;
+                    cell.value.IsCrossed = false;
+                }
+            }
+        }
     }
 }
