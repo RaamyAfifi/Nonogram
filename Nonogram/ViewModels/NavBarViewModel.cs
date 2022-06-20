b61da88d5f4ecadb8b0248789edc3f4509fbe25b
maartjeA2001 <55484829+maartjeA2001@users.noreply.github.com>
2022-06-20 12:46:34 +0200

    add navigation logic

--- Nonogram/ViewModels/NavBarViewModel.cs
+++ Nonogram/ViewModels/NavBarViewModel.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Microsoft.Toolkit.Mvvm.Input;
+using Nonogram.Pages;
 
 namespace Nonogram.ViewModels
 {
     public class NavBarViewModel
     {
+        public ICommand AllPuzzleCommand { get; set; }
+        public ICommand CreatePuzzleCommand { get; set; }
+
+        public NavBarViewModel()
+        {
+            AllPuzzleCommand = new RelayCommand(GotoAllPuzzles);
+            CreatePuzzleCommand = new RelayCommand(GotoCreatePuzzle);
+        }
+
+        public static void GotoAllPuzzles()
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+            rootFrame.Navigate(typeof(OverViewPage));
+        }
+
+        public static void GotoCreatePuzzle()
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+            rootFrame.Navigate(typeof(CreatePage));
+        }
     }
 }
