c3c06503247bf9570b9ae814431ffb62f5db7172
maartjeA2001 <55484829+maartjeA2001@users.noreply.github.com>
2022-06-20 21:48:40 +0200

    add logic solvePage
    Co-Authored-By: RaamyAfifi <56026512+RaamyAfifi@users.noreply.github.com>

--- Nonogram/ViewModels/SolvePageViewModel.cs
+++ Nonogram/ViewModels/SolvePageViewModel.cs
@@ -6,12 +6,13 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Windows.UI;
 using Microsoft.Toolkit.Mvvm.Input;
 using NonogramModels;
 
 namespace Nonogram.ViewModels
 {
-    public class SolvePageViewModel
+    public class SolvePageViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
         public List<List<int>> RowHints { get; set; }
@@ -25,10 +26,25 @@ namespace Nonogram.ViewModels
         {
             SolvingPuzzle = new SolvingPuzzle(puzzle);
             Dimensions = SolvingPuzzle.Puzzle.XAxis + "/" + SolvingPuzzle.Puzzle.YAxis;
+            DoneCommand = new RelayCommand(validatePuzzle);
             ColumnHints = SolvingPuzzle.SolvedPuzzle.GetAllColumnHints();
             RowHints = SolvingPuzzle.SolvedPuzzle.GetAllRowHints();
         }
 
+        /// <summary>
+        /// Validate puzzle solution
+        /// If the solution is right color the puzzle in the solution's color
+        /// Also changes the button text and command of the button to go back the the overview
+        /// </summary>
+        private void validatePuzzle()
+        {
+            if (SolvingPuzzle.ValidateSolution())
+            {
+                SolvingPuzzle.ColorSolution();
+                ButtonText = "Back to overview";
+                DoneCommand = new RelayCommand(NavBarViewModel.GotoAllPuzzles);
+            }
+        }
         public SolvingPuzzle SolvingPuzzle
         {
             get => solvingPuzzle;
@@ -58,6 +74,38 @@ namespace Nonogram.ViewModels
                 NotifyPropertyChanged();
             }
         }
+
+        /// <summary>
+        /// Command for crossing off the cell
+        /// Erases cross if cell is crossed
+        /// </summary>
+        public ICommand CrossCellCommand { get; } = new RelayCommand<Cell>(cell => {
+            if (cell.IsCrossed)
+            {
+                cell.IsCrossed = false;
+            }
+            else if (!cell.IsColored)
+            {
+                cell.IsCrossed = true;
+            }
+        });
+
+        /// <summary>
+        /// Command for coloring the cell
+        /// Erases color if cell is colored
+        /// </summary>
+        public ICommand ColorCellCommand { get; } = new RelayCommand<Cell>(cell => {
+            if (cell.IsColored && !cell.IsCrossed)
+            {
+                cell.RGBA = Colors.White;
+                cell.IsColored = false;
+            }
+            else if (!cell.IsCrossed)
+            {
+                cell.RGBA = Colors.Black;
+                cell.IsColored = true;
+            }
+        });
 
         private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
         {
